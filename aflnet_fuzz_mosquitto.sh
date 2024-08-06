e6dce7329a362a05cc93345396e26a4f7526dd75
ItsMagick <magick930@googlemail.com>
2024-08-06 13:43:52 +0200

    - changed the aflnet_fuzz_mosquitto.sh script to not capture traffic

--- aflnet_fuzz_mosquitto.sh
+++ aflnet_fuzz_mosquitto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 set -ex
-./binaries/afl-fuzz -d -m none -i ./aflnet/tutorials/mosquitto/in-mqtt -o out -N tcp://127.0.0.1/1883 -P MQTT -D 10000 -q 3 -s 3 -E -K -R ./binaries/mosquitto &
-./capture_traffic.sh
+./binaries/afl-fuzz -d -m none -i ./aflnet_in -o out -N tcp://127.0.0.1/1883 -P MQTT -D 10000 -q 3 -s 3 -E -K -R ./binaries/mosquitto
